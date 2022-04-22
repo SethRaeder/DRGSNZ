fe3a099bb684c78c6b1e1635ec6c67f55b4a4d94
Tristan Calay <sethraeder@gmail.com>
2022-04-21 23:49:48 -0400

    rename index

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-import { SVG } from 'http://svg.js/'
-const canvas = document.querySelector("canvas");
-const c = canvas.getContext("2d");
-
-canvas.width = 1024;
-canvas.height = 576;
-
-c.fillRect(0, 0, canvas.width, canvas.height)
-
-class Sprite {
-    //Class for canvas sprites
-    constructor(position) {
-        this.position = position;
-    }
-
-    draw() {
-
-    }
-}
-
-const player = new Sprite({
-    x: 0,
-    y: 0
-});
-
-console.log("Hello")
-
-var draw = SVG().addTo('body').size(300, 300)
-var rect = draw.rect(100, 100).attr({ fill: '#f06' })
+var draw = SVG().addTo('body').size(600, 600)
+var rect = draw.image('assets/charZephyr/preview/previewIdle.svg')
+rect.animate(2000, 1000, 'now').move(150, 150)
+rect.animate(1000, 2000, 'now').move(0, 0)
