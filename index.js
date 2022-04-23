c46ec0529eff1cc960c8f5bc1ab33a848a62a038
Tristan Calay <sethraeder@gmail.com>
2022-04-22 23:50:18 -0400

    keypress test

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class SpriteSheet {
+    constructor(spritesArr) {
+        this.spritesArr = spritesArr
+        this.spriteIndex = 0
+    }
+    draw(canvas) {
+        try {
+            this.curElement.remove()
+        } catch (error) {
+
+        }
+
+        this.curElement = canvas.image(this.spritesArr[this.spriteIndex]);
+    }
+
+    nextSprite() {
+        this.spriteIndex += 1;
+        console.log(this.spriteIndex)
+        if (this.spriteIndex > this.spritesArr.length - 1) {
+            this.spriteIndex = 0;
+        }
+    }
+}
+
+var charZephyr = new SpriteSheet(['assets/charZephyr/earFar.svg',
+    'assets/charZephyr/earClose.svg',
+    'assets/charZephyr/headIdle.svg',
+    'assets/charZephyr/iris.svg',
+])
+
 var draw = SVG().addTo('body').size(600, 600)
-var rect = draw.image('assets/charZephyr/preview/previewIdle.svg')
-rect.animate(2000, 1000, 'now').move(150, 150)
-rect.animate(1000, 2000, 'now').move(0, 0)
+charZephyr.draw(draw)
+
+document.addEventListener('keydown', function(event) {
+
+    if (event.code == 'ArrowRight') {
+        charZephyr.nextSprite()
+        charZephyr.draw(draw)
+        console.log('hello')
+    }
+}, true)
