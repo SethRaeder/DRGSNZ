9307c6e677e8803c466284a893cd4b86d3f210e4
Tristan Calay <sethraeder@gmail.com>
2022-04-26 15:05:43 -0400

    fetch previewsprites

--- index.js
+++ index.js
@@ -267,6 +267,7 @@ await pupilSprites.fetchElements(svgDraw)
 await eyeSprites.fetchElements(svgDraw)
 await earCloseSprites.fetchElements(svgDraw)
 await nostrilSprites.fetchElements(svgDraw)
+await previewSprites.fetchElements(svgDraw)
 await toolSprites.fetchElements(svgDraw)
 console.log('svgs loaded')
 
